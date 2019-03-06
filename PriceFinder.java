d1c18ce86ae6751e0ef32861c78f4b4610f806cf
elijahpele <34014559+elijahpele@users.noreply.github.com>
2019-03-05 19:05:31 -0700

    Add files via upload

---
+++ PriceFinder.java
@@ -0,0 +1,13 @@
+package pricewatcher_homework2.control;
+
+import java.util.Random;
+
+public class PriceFinder {
+
+    public double livePrice(String url) {
+        Random random = new Random();
+        double simulatedNewPrice = 250.00 + (350.00 - 250.00) * random.nextDouble(); //Later, this will come from an actual website.
+        simulatedNewPrice = Math.round(simulatedNewPrice * 100.0) / 100.0;
+        return simulatedNewPrice;
+    }
+}

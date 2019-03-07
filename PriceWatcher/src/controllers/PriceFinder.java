f1fa6e232d38372de3b3cb72f3192d96108e1081
ebarragan3 <ebarragan3@Miners.utep.edu>
2019-03-06 21:54:06 -0700

    Update PriceFinder.java

--- PriceWatcher/src/controllers/PriceFinder.java
+++ PriceWatcher/src/controllers/PriceFinder.java
@@ -1,14 +1,20 @@
 package controllers;
-
+import models.Item;
 import java.util.Random;
 
 public class PriceFinder {
+    private Item item;
+    
+    public PriceFinder(){}
+    
+    public  PriceFinder(Item item){
+        this.item = item;
+    }
 
-	public double livePrice(String url) {
-		Random random = new Random();
-		double simulatedNewPrice = 250.00 + (350.00 - 250.00) * random.nextDouble(); // Later, this will come from an
-																						// actual website.
-		simulatedNewPrice = Math.round(simulatedNewPrice * 100.0) / 100.0;
-		return simulatedNewPrice;
-	}
+    public double livePrice(String url) {
+        Random random = new Random();
+        double simulatedNewPrice = 250.00 + (350.00 - 250.00) * random.nextDouble(); //Later, this will come from an actual website.
+        simulatedNewPrice = Math.round(simulatedNewPrice * 100.0) / 100.0;
+        return simulatedNewPrice;
+    }
 }
